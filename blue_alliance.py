ce954763057c4807434963f38784478c77f354e4
Iker Fuentes <A01749675@tec.mx>
2024-10-23 08:44:49 -0600

    Pub

--- blue_alliance.py
+++ blue_alliance.py
@@ -4,27 +4,18 @@ from pprint import pprint
 URL = "https://www.thebluealliance.com/"
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup.prettify())
-results = soup.find("table")
-
-events = results.find_all("tr")
-
-result = {}
-
-for event in events:
-    
-    data = event.find("td")
-    if data: 
-        print("_________________--")
-        print(data)
-        a = data.find("a")
-        direction = data.find("small")
-        result[a["title"]] = (a["title"],URL+a["href"][1:],direction.text)
-
-for r in result:
-    print(f"Event: {result[r][0]}, URL: {result[r][1]}, Direction: {result[r][2]}")
-        
+def getRegionalData()->dict:
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("table")
+    events = results.find_all("tr")
+    result = {}
+    for event in events:
+        data = event.find("td")
+        if data: 
+            a = data.find("a")
+            direction = data.find("small")
+            result[a["title"]] = (a["title"],URL+a["href"][1:],direction.text)
+    return result
 
 
