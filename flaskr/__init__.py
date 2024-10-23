56d0f812b849d61fd023cbeda4496d17b26e0838
Alan Vega <a01750658@tec.mx>
2024-10-23 09:19:52 -0600

    Cambios a init.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,7 +12,7 @@ def create_app(test_config = None):
     if test_config is None:
         app.config.from_pyfile('config.py', silent = True)
     else:
-        app.config.from_pyfile('config.py', silent = True)
+        app.config.from_mapping(test_config)
     
     try:
         os.makedirs(app.instance_path)
