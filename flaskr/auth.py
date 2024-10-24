cd7d6e12478f346916ff17213f3618445e8ed302
Ulises Jaramillo <ulises.jp@outlook.com>
2024-10-23 22:30:50 -0600

    Correction of selected preferences

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,104 +1,118 @@
 import functools
 
+
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+   Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 from flaskr.db import get_db
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    if request.method == 'POST':
-        username,password,gundam,regional,fmf = None,None,None,None,None
-        
+   if request.method == 'POST':
+       username = request.form['username']
+       password = request.form['password']
+       gundam = 'gundam' in request.form
+       regional = 'regional' in request.form
+       fmf = 'fmf' in request.form
 
-        try:
-            username = request.form['username']
-            password = request.form['password']
-            gundam = request.form.get('gundam',0)
-            regional = request.form.get('regional',0)
-            fmf = request.form.get('fmf',0)
-        except:
-            pass
 
-        db = get_db()
-        error = None
+       db = get_db()
+       error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
 
-        gundam = 1 if gundam else 0
-        regional = 1 if regional else 0
-        fmf = 1 if fmf else 0
-        print(f"username: {username}, password: {password}, gundam: {gundam}, regional: {regional}, fmf: {fmf}")
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password,gundam,regional,fmf) VALUES (?, ?,?,?,?)",
-                    (username, generate_password_hash(password),gundam,regional,fmf),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
+       if not username:
+           error = 'Username is required.'
+       elif not password:
+           error = 'Password is required.'
+       elif db.execute(
+           'SELECT id FROM user WHERE username = ?', (username,)
+       ).fetchone() is not None:
+           error = f'User {username} is already registered.'
 
-        flash(error)
 
-    return render_template('auth/register.html')
+       if error is None:
+           try:
+               db.execute(
+                   "INSERT INTO user (username, password, gundam, regional, fmf) VALUES (?, ?, ?, ?, ?)",
+                   (username, generate_password_hash(password), gundam, regional, fmf),
+               )
+               db.commit()
+           except db.IntegrityError:
+               error = f"User {username} is already registered."
+           else:
+               return redirect(url_for("auth.login"))
+
+
+       flash(error)
+
+
+   return render_template('auth/register.html')
+
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+   if request.method == 'POST':
+       username = request.form['username']
+       password = request.form['password']
+       db = get_db()
+       error = None
+       user = db.execute(
+           'SELECT * FROM user WHERE username = ?', (username,)
+       ).fetchone()
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
 
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+       if user is None:
+           error = 'Incorrect username.'
+       elif not check_password_hash(user['password'], password):
+           error = 'Incorrect password.'
 
-        flash(error)
 
-    return render_template('auth/login.html')
+       if error is None:
+           session.clear()
+           session['user_id'] = user['id']
+           return redirect(url_for('index'))
+
+
+       flash(error)
+
+
+   return render_template('auth/login.html')
+
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+   user_id = session.get('user_id')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+
+   if user_id is None:
+       g.user = None
+   else:
+       g.user = get_db().execute(
+           'SELECT * FROM user WHERE id = ?', (user_id,)
+       ).fetchone()
+
 
 @bp.route('/logout')
 def logout():
-    session.clear()
-    return redirect(url_for('index'))
+   session.clear()
+   return redirect(url_for('index'))
+
 
 def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
+   @functools.wraps(view)
+   def wrapped_view(**kwargs):
+       if g.user is None:
+           return redirect(url_for('auth.login'))
 
-        return view(**kwargs)
 
-    return wrapped_view
+       return view(**kwargs)
+
+
+   return wrapped_view
--- flaskr/data.py
+++ flaskr/data.py
@@ -1,167 +1,129 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+   Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 import sys
-from .web_scrapers.data_getter import GetAllContent 
+from .web_scrapers.data_getter import GetAllContent
 
 
 bp = Blueprint('data', __name__)
 
-#@bp.route('/index', methods=('GET', 'POST'))
-def initialize_data():
-    all = GetAllContent()
-    dba,fmf,gundam = all['blue_alliance'], all['fmf_news'], all['gundam']
 
-    db = get_db()
-    db.execute(
-        '''
-        DELETE FROM regional;
-        '''
-    )
-    db.execute(
-        '''
-        DELETE FROM fmf_news;
-        '''
-    )
-    db.execute(
-        '''
-        DELETE FROM gundam;
-        '''
-    )
-    for item in dba:
-        db.execute(
-            '''
-            INSERT INTO regional (event, url, direction)
-            VALUES (?, ?, ?);
-            ''',
-            (item['event'], item['url'], item['direction'])
-        )
-    for item in fmf:
-        db.execute(
-            '''
-            INSERT INTO fmf_news (title, description, date, link)
-            VALUES (?, ?, ?, ?);
-            ''',
-            (item['title'], item['description'], item['date'], item['link'])
-        )
-    for item in gundam:
-        db.execute(
-            '''
-            INSERT INTO gundam (title)
-            VALUES (?);
-            ''',
-            (item,)
-        )
-    db.commit()
+def initialize_data():
+   all = GetAllContent()
+   dba, fmf, gundam = all['blue_alliance'], all['fmf_news'], all['gundam']
+
+
+   db = get_db()
+   db.execute('DELETE FROM regional;')
+   db.execute('DELETE FROM fmf_news;')
+   db.execute('DELETE FROM gundam;')
+
+
+   for item in dba:
+       db.execute(
+           '''
+           INSERT INTO regional (event, url, direction)
+           VALUES (?, ?, ?);
+           ''',
+           (item['event'], item['url'], item['direction'])
+       )
+   for item in fmf:
+       db.execute(
+           '''
+           INSERT INTO fmf_news (title, description, date, link)
+           VALUES (?, ?, ?, ?);
+           ''',
+           (item['title'], item['description'], item['date'], item['link'])
+       )
+   for item in gundam:
+       db.execute(
+           '''
+           INSERT INTO gundam (title)
+           VALUES (?);
+           ''',
+           (item,)
+       )
+   db.commit()
+
 
 @bp.route('/')
 def index():
-    initialize_data()
-    db = get_db()
-    titles =[
-        "",
-        "",
-        ""
-        
-    ]
+   initialize_data()
+   db = get_db()
+   titles = []
+   dba, fmf, gundam = [], [], []
 
-    if(g.user):
-        if(g.user['regional'] and g.user["regional"]==1):
-            dba = db.execute(
-                '''
-                SELECT * FROM regional;
-                '''
-            )
-            titles[0] = "Robotics"
-        else:
-            dba = []
-            titles[0] = ""
-        if(g.user['fmf'] and g.user["fmf"]==1):
-            fmf = db.execute(
-                '''
-                SELECT * FROM fmf_news;
-                '''
-            ).fetchall()
-            titles[1] = "Sports"
-        else:
-            fmf = []
-            titles[1] = ""
-        if(g.user['gundam'] and g.user["gundam"]==1):
-            print(g.user['gundam']) 
-            gundam = db.execute(
-                '''
-                SELECT * FROM gundam;
-                '''
-            ).fetchall()
-           
-            titles[2] = "Anime"
-        else:
-            titles[2] = ""
-            gundam = []
-            
-    else:
-        print("No user")
-        titles= ["Robotics","Sports","Anime"]
-        dba = db.execute(
-            '''
-            SELECT * FROM regional;
-            '''
-        ).fetchall()
-        fmf = db.execute(
-            '''
-            SELECT * FROM fmf_news;
-            '''
-        ).fetchall()
-        gundam = db.execute(
-            '''
-            SELECT * FROM gundam;
-            '''
-        ).fetchall()
-        print(titles)
 
-    return render_template('main/index.html', dba = dba, fmf = fmf, gundam = gundam,titles = titles)
+   if g.user:
+       if g.user['regional']:
+           dba = db.execute('SELECT * FROM regional;').fetchall()
+           titles.append("Robotics")
+       if g.user['fmf']:
+           fmf = db.execute('SELECT * FROM fmf_news;').fetchall()
+           titles.append("Sports")
+       if g.user['gundam']:
+           gundam = db.execute('SELECT * FROM gundam;').fetchall()
+           titles.append("Gundam")
+   else:
+       titles = ["Robotics", "Sports", "Gundam"]
+       dba = db.execute('SELECT * FROM regional;').fetchall()
+       fmf = db.execute('SELECT * FROM fmf_news;').fetchall()
+       gundam = db.execute('SELECT * FROM gundam;').fetchall()
 
-@bp.route('/dba', methods=('GET',))
+
+   return render_template('main/index.html', dba=dba, fmf=fmf, gundam=gundam, titles=titles)
+
+
+@bp.route('/dba', methods=('GET', 'POST'))
 @login_required
 def dba():
-    db = get_db()
-    dba = db.execute(
-        '''
-        SELECT * FROM regional;
-        '''
-    ).fetchall()
-    return render_template('main/dba.html', posts = dba)
-@bp.route('/fmf', methods=('GET',))
+   if not g.user['regional']:
+       return render_template('main/subscribe.html', category='regional')
+  
+   db = get_db()
+   dba = db.execute('SELECT * FROM regional;').fetchall()
+   return render_template('main/dba.html', posts=dba)
+
+
+@bp.route('/fmf', methods=('GET', 'POST'))
 @login_required
 def fmf():
-    db = get_db()
-    fmf = db.execute(
-        '''
-        SELECT * FROM fmf_news;
-        '''
-    ).fetchall()
-    return render_template('main/fmf.html', posts = fmf)
+   if not g.user['fmf']:
+       return render_template('main/subscribe.html', category='fmf')
+  
+   db = get_db()
+   fmf = db.execute('SELECT * FROM fmf_news;').fetchall()
+   return render_template('main/fmf.html', posts=fmf)
 
-@bp.route('/gundam', methods=('GET',))
+
+@bp.route('/gundam', methods=('GET', 'POST'))
 @login_required
 def gundam():
-    db = get_db()
-    gundam = db.execute(
-        '''
-        SELECT * FROM gundam;
-        '''
-    ).fetchall()
-    return render_template('main/gundam.html', posts = gundam)
+   if not g.user['gundam']:
+       return render_template('main/subscribe.html', category='gundam')
+  
+   db = get_db()
+   gundam = db.execute('SELECT * FROM gundam;').fetchall()
+   return render_template('main/gundam.html', posts=gundam)
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+
+@bp.route('/subscribe/<category>', methods=('POST',))
 @login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('blog.index'))
+def subscribe(category):
+   db = get_db()
+   db.execute(
+       f'UPDATE user SET {category} = 1 WHERE id = ?', (g.user['id'],)
+   )
+   db.commit()
+  
+   # Recargar el usuario actualizado en g.user
+   g.user = db.execute(
+       'SELECT * FROM user WHERE id = ?', (g.user['id'],)
+   ).fetchone()
+  
+   return redirect(url_for('index'))
