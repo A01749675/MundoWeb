769f06496ec4de111bbca80f5c9eeb5ecbbf9b11
Iker Fuentes <A01749675@tec.mx>
2024-10-23 22:26:33 -0600

    robotics

--- flaskr/data.py
+++ flaskr/data.py
@@ -105,7 +105,7 @@ def index():
             
     else:
         print("No user")
-        titles= ["Regional","Sports","Anime"]
+        titles= ["Robotics","Sports","Anime"]
         dba = db.execute(
             '''
             SELECT * FROM regional;
